23e40668a1fb382a78bd01a886742aa10bc4f95e
banelmikhael <banel.mikhael.bm@gmail.com>
2022-11-13 14:48:20 +0300

    added footer(without links to webSite)/ added logo and director in single movie pagewith similar movies and actors in the movie

--- script.js
+++ script.js
@@ -156,22 +156,19 @@ const movieDetails = async (movie) => {
   renderMovie(movieRes, movieCredits, movieTrailer.results, movieSimilar.results);
 };
 
+const fetchgenre = async (url) => {
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    renderMovies(data.results)
+  })
+}
 // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
-
 const fetchMovies = async (movieFilter) => {
   const url = constructUrl(`movie/${movieFilter}`);
   const res = await fetch(url);
   return res.json();
 };
-
-const fetchgenre = async (url) => {
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      renderMovies(data.results)
-    })
-}
-
 // Don't touch this function please. This function is to fetch one movie.
 const fetchMovie = async (movieId) => {
   const url = constructUrl(`movie/${movieId}`);
@@ -184,11 +181,13 @@ const renderMovies = (movies) => {
   // console.log( movies)
   movies.map((movie) => {
     const movieDiv = document.createElement("div");
-    movieDiv.setAttribute('class', ' lg:ml-10 bg-gradient-to-r  from-blue-100 hover:opacity-[50%] via-blue-300 to-blue-500 text text-2xl text-center max-w-sm rounded overflow-hidden shadow-lg cursor-pointer')
+    movieDiv.setAttribute('class', 'lg:ml-10 bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900 hover:opacity-[25%] text-2xl text-center max-w-sm rounded overflow-hidden shadow-lg cursor-pointer')
     movieDiv.innerHTML = `
     <img class="w-full" src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="Sunset in the mountains">
   <div class="px-2 py-2">
-    <div class="font-bold text-xl mb-2">${movie.title}</div>
+    <div class="font-bold text-xl mb-2">${movie.title}
+    
+    </div>
   
   </div>
   
@@ -202,34 +201,48 @@ const renderMovies = (movies) => {
 };
 
 // You'll need to play with this function in order to add features and enhance the style.
-const renderMovie = (movie, movieCast, videos, similar) => {
+const renderMovie = (movie, movieCast, videos, similarMovies) => {
   CONTAINER.setAttribute('class', `grid grid-cols-3 grid-rows-3 min-h-screen  text-black`)
   CONTAINER.style.background = `linear-gradient(rgb(255 255 255 / 90%), rgb(255 255 255 / 40%)), url(${BACKDROP_BASE_URL + movie.backdrop_path}) no-repeat `
   CONTAINER.style.backgroundSize = "cover"
 
   CONTAINER.innerHTML = `
-
   <div>
-  <iframe class="col-statrt-1 col-end-2  h-[400px] w-[400px] border-2 pt-[20px] pl-[20px] rounded-br-[30%] rounded-tl-[30%]" src="https://www.youtube.com/embed/${videos.length === 0 ? videos.key : videos[0].key}" 
-  frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-   allowfullscreen></iframe>;
-  </div>
-        <div class="col-start-2 col-end-4 row-start-2 ">
-            <h2 id="movie-title" class="text-[50px]">${movie.title}</h2>
-            <p id="movie-release-date" class="pt-[30px]"><b>Release Date:</b> ${movie.release_date
-    }</p>
-            <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p>
-            <h3>Overview:</h3>
-            <p id="movie-overview">${movie.overview}</p>
-       
-        </div>
-            <h3 class="col-span-3 row-start-3 text-[30px] pl-[50px] " >Actors: <ul class="actorList"> </ul></h3>
-            
-           
+  <iframe
+      class="col-statrt-1 col-end-2  h-[400px] w-[400px] border-2 pt-[20px] pl-[20px] rounded-br-[30%] rounded-tl-[30%]"
+      src="https://www.youtube.com/embed/${videos.length === 0 ? videos.key : videos[0].key}" frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>;
+</div>
+<div class="col-start-2 col-end-4 row-start-1 flex flex-col ">
+  <p class="pt-[300px] pl-[3px] "><span class="font-bold">Movie Language:</span>
+      ${movie.original_language.toUpperCase()} </p>
+  <p class=" pl-[3px]"> <b>Rating :</b> &nbsp${movie.vote_average} <i class="fas fa-star"></i>&nbsp&nbsp</p>
+  <p class=" pl-[3px]"><b>Vote Count : </b>${movie.vote_count} </p>
+  <p class=" pl-[3px]"><b>Runtime:</b> ${movie.runtime} Minutes</p>
+</div>
+<div class="logoDiv col-start-1 col-end-2 row-start-2"> </div>
+<div class="col-start-2 col-end-4 row-start-2 ">
+  <h2 id="movie-title" class="text-[50px]">${movie.title}</h2>
+  <h3>Overview:</h3>
+  <p id="movie-overview">${movie.overview}</p>
+</div>
+<h3 class="col-span-3 row-start-3 text-[30px] pl-[50px] ">Actors: <ul class="actorList"> </ul>
+</h3>
+<div class="similar-movies    lg:col-start-1 lg:col-end-5 lg:row-start-4 lg:flex pr-[30px]">
+</div>
       `;
 
 
+  movie.production_companies.slice(0, 1).forEach(movieLogo => {
 
+    const logoDiv = document.querySelector(".logoDiv")
+    const createDiv = document.createElement("div")
+    createDiv.innerHTML = `<img class=" h-[300px] w-[400px]  " src="${PROFILE_BASE_URL + movieLogo.logo_path}"> `
+
+    logoDiv.appendChild(createDiv)
+
+    console.log(PROFILE_BASE_URL + movieLogo.logo_path)
+  })
   const actorList = document.querySelector(".actorList")
 
   actorList.setAttribute('class', 'flex flex-row justify-center pt-[100px] gap-x-[12px]')
@@ -237,25 +250,39 @@ const renderMovie = (movie, movieCast, videos, similar) => {
   movieCast.cast.slice(0, 5).forEach(actor => {
     // let castImgUrl = actor.profile_path
     // console.log("hola senior", castImgUrl)
-    const actorCastDiv = document.createElement("div");
-    actorCastDiv.setAttribute("class","pl-[100px] cursor-pointer")
 
+    const actorCastDiv = document.createElement("div");
+    actorCastDiv.setAttribute("class", "pl-[100px] cursor-pointer")
     actorCastDiv.innerHTML = `
-   <img class="rounded-[30%] h-[125px] w-[12 5px]" src="${PROFILE_BASE_URL+ actor.profile_path}" > 
+   <img class="rounded-[30%] h-[125px] w-[12 5px]" src="${PROFILE_BASE_URL + actor.profile_path}" > 
     <p>${actor.name} </p> `
 
     actorList.appendChild(actorCastDiv);
-    console.log(actor)
+
   })
-  console.log(movieCast)
+  // console.log(movieCast)
   // for (let i=0;i<=5;i++){
   //   const actor =document.createElement("li");
   //   actor.innerHTML=`${"'"+movieCast.cast[i].profile_path+"'"}`
   //   actorList.appendChild(actor);
   //   console.log(movieCast.cast[i]) 
   // }
-
-
+  similarMovies.slice(0, 6).forEach(singleMoive => {
+    const similarMoviesDiv = document.querySelector(".similar-movies")
+    const similarDiv = document.createElement("div")
+    similarDiv.setAttribute("class", " w-[50%] h-[50%] mb-[200px] lg:ml-10 bg-gradient-to-r from-slate-500 to-yellow-100 hover:opacity-[25%] text-2xl text-center max-w-sm rounded overflow-hidden shadow-lg cursor-pointer")
+    similarDiv.innerHTML = ` 
+    <img class="w-full " src="${BACKDROP_BASE_URL + singleMoive.backdrop_path}" alt="">
+     <div class="px-2 py-2">
+       <div class="font-bold text-xl mb-2">${singleMoive.title}
+       </div>
+     </div>`
+    similarMoviesDiv.appendChild(similarDiv)
+    similarDiv.addEventListener("click", () => {
+      movieDetails(singleMoive);
+      // console.log(movieDetails(singleMoive))  
+    })
+  })
 };
 
 document.addEventListener("DOMContentLoaded", autorun);
